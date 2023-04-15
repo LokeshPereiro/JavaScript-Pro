4285fc4a863d268d6c5b73f3cdc58547f166f4ae
Lokesh Pereiro <lpdraft001@gmail.com>
2023-04-15 16:17:24 +0200

    readme file add so that we have quick view of app progress

--- Proyectos/TodoApp-vite/main.js
+++ Proyectos/TodoApp-vite/main.js
@@ -1,11 +1,8 @@
 import "./style.css";
-import { App } from "./src/todos/app";
+import { initApp } from "./src/todos/app";
 import todoStore from "./src/store/todoStore";
 
 todoStore.initStore();
 
 // Main function to run a TodoApp
-/**
- * @param {String} #app --> index.html <div id="app"></div>
- */
-App("#app");
+initApp();
