f0401fc6d0fa9da22b9928d1605d4f9ed876eff3
Lokesh Pereiro <lpdraft001@gmail.com>
2023-04-15 14:44:31 +0200

    printing list of todos

--- Proyectos/TodoApp-vite/main.js
+++ Proyectos/TodoApp-vite/main.js
@@ -1,5 +1,11 @@
 import "./style.css";
 import { App } from "./src/todos/app";
 import todoStore from "./src/store/todoStore";
+
 todoStore.initStore();
+
+// Main function to run a TodoApp
+/**
+ * @param {String} #app --> index.html <div id="app"></div>
+ */
 App("#app");
